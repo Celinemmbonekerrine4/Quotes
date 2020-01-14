620a67db8e9aa731ed6a9a40840ba201b6b0d78b
Celinemmbonekerrine4 <celinekerrine@gmail.com>
2020-01-14 12:00:09 +0300

    it will create quote-detail component

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -1,37 +1,37 @@
-import { Component, OnInit, Input,Output,EventEmitter } from '@angular/core';
-import { Quote } from '@angular/compiler';
+// import { Component, OnInit, Input,Output,EventEmitter } from '@angular/core';
+// import { Quote } from '@angular/compiler';
 
-@Component({
-  selector: 'app-quote-detail',
-  templateUrl: './quote-detail.component.html',
-  styleUrls: ['./quote-detail.component.css']
-})
-export class QuoteDetailComponent implements OnInit {
+// @Component({
+//   selector: 'app-quote-detail',
+//   templateUrl: './quote-detail.component.html',
+//   styleUrls: ['./quote-detail.component.css']
+// })
+// export class QuoteDetailComponent implements OnInit {
 
-  @Input() quote :Quote;
+//   @Input() quote :Quote;
 
-  @Output()isComplete = new EventEmitter<boolean>();
+//   @Output()isComplete = new EventEmitter<boolean>();
 
-  quoteComplete(complete:boolean){
-    this.isComplete.emit(complete);
-  }
-  quoteDelete(complete:boolean){
-    this.isComplete.emit(complete);
-  }
+//   quoteComplete(complete:boolean){
+//     this.isComplete.emit(complete);
+//   }
+//   quoteDelete(complete:boolean){
+//     this.isComplete.emit(complete);
+//   }
 
-  upvote=0;
-  downvote=0;
+//   upvote=0;
+//   downvote=0;
 
-  voteDown(){
-    this.downvote=this.downvote+1
-  };
-  voteUp(){
-    this.upvote=this.upvote+1
-  };
+//   voteDown(){
+//     this.downvote=this.downvote+1
+//   };
+//   voteUp(){
+//     this.upvote=this.upvote+1
+//   };
   
-  constructor() { }
+//   constructor() { }
 
-  ngOnInit() {
-  }
+//   ngOnInit() {
+//   }
 
-}
+// }
